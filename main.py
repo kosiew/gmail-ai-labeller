2e26124c05af60a8fdc12ee653537c4d2081b3dc
Siew Kam Onn <kosiew@gmail.com>
2025-01-24 10:24:36 +0800

    Revert "Update GPT model to Phi-3-mini-4k-instruct"
    This reverts commit 31f590f31aba20227faae8e29c5a484c2c4415a0.

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 # ----- Constants -----
-# GPT4ALL_MODEL = "mistral-7b-instruct-v0"
-GPT4ALL_MODEL = "Phi-3-mini-4k-instruct"
+GPT4ALL_MODEL = "mistral-7b-instruct-v0"
 LABELS = ["programming", "news", "machine_learning", "etc"]
 LABEL_PROCESSED = "processed"
 OLDER_THAN = "30d"
